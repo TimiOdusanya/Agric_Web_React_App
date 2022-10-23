fd90dc7c8b25cf7536a37b8fc8cf12453ad0be46
Timi Odusanya <timmycruz36@gmail.com>
2022-10-23 13:09:04 +0100

    Added new changes to git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {Routes, Route} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Buy from './components/Buy';
+import Sell from "./components/Sell"
+import Location from "./components/Location"
+import Invest from "./components/Invest"
+import Content from './components/Content';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="buy" element={<Buy />} />
+          <Route path="sell" element={<Sell />} />
+          <Route path="invest" element={<Invest />} />
+          <Route path="location" element={<Location />} />
+          <Route path="content" element={<Content />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
